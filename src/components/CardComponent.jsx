17e5b08403609cbfc6390f1608e66c50bf385897
Frankam487 <fkamgang06@gmail.com>
2024-10-17 12:00:08 +0100

    portfolio feature

---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,7 @@
+// export const CardComponent = () => {
+//     return ( 
+//         <div className="cardeComponent">
+
+//         </div>
+//      );
+// }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,90 +1,60 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-const Portfolio = () => {
-  return (
-    <div className="portfolio">
-      <div className="text flex flex-col items-center">
-        <h3>Mon Portfolio</h3>
-        <h1>Projets Recents</h1>
-      </div>
-      <ul className="flex items-center w-[70%] mx-auto ">
-        <li className="mx-auto my-10">
-          <a href="">Tous les Projets</a>
-        </li>
-        <li className="">
-          <a href="">Web Design</a>
-        </li>
-        <li className="mx-auto">
-          <a href="">App Mobile</a>
-        </li>
-        <li className="mx-auto">
-          <a href="">Grahique Design</a>
-        </li>
-      </ul>
-      <div className="projects flex flex-wrap items-center justify-center">
-        <div className="image m-12 relative w-[400px]">
-          <img src="../../public/fr.jpg" alt="" className="w-full" />
-          <div className="">
-            <div className="btns ">
-              <button className="flex">X</button>
-              <button className="flex">X</button>
-            </div>
-            <h2 className="">Web Design</h2>
-          </div>
-        </div>
-        <div className="image m-12 relative w-[400px] ">
-          <img src="../../public/fr.jpg" alt="" />
-          <div className="content ">
-            <div className="btns ">
-              <button>X</button>
-              <button>X</button>
-            </div>
-            <h2 className="">App Mobile</h2>
-          </div>
-        </div>
-        <div className="image m-12 relative w-[400px]">
-          <img src="../../public/fr.jpg" alt="" class />
-          <div className="content ">
-            <div className="btns ">
-              <button>X</button>
-              <button>X</button>
-            </div>
-            <h2 className="translate-y-[210px]">Graphic Design</h2>
-          </div>
-        </div>
-        <div className="image m-12 relative w-[400px]">
-          <img src="../../public/fr.jpg" alt="" class />
-          <div className="content ">
-            <div className="btns ">
-              <button className="">X</button>
-              <button className="">X</button>
-            </div>
-            <h2 className="translate-y-[210px]">Web Design</h2>
-          </div>
-        </div>
-        <div className="image m-12 relative w-[400px]">
-          <img src="../../public/fr.jpg" alt="" class />
-          <div className="content ">
-            <div className="btns ">
-              <button>X</button>
-              <button>X</button>
-            </div>
-            <h2 className="translate-y-[210px]">Web Design</h2>
-          </div>
-        </div>
-        <div className="image m-12 relative w-[400px]">
-          <img src="../../public/fr.jpg" alt="" />
-          <div className="content absolute top-[-220px] left-[-11px] h-full w-full bg-black/60 flex flex-col justify-center items-center opacity-0 transition duration-600 md:top-[-220px]">
-            <div className="btns transform translate-y-[35px] transition-transform duration-600 group-hover:translate-y-0">
-              <button className="">X</button>
-              <button className="">X</button>
-            </div>
-            <h2 className="translate-y-[210px]">Web Design</h2>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
+const Portfolio = () => {
+  
+  return ( 
+    <div className="portfolio">
+     <div className="text">
+      <h2>Mon Portfolio</h2>
+      <h1>Projets Recents</h1>
+     </div>
+     <div className="menu">
+      <ul>
+        <li>Projets</li>
+        <li>Web Design</li>
+        <li>App Mobile</li>
+        <li>Design Graphic</li>
+      </ul>
+     </div>
+     <div className="projects-container">
+    <div className="card">
+      <img src="../../fr.jpg" alt="" />
+      <button className="btn1"><span className="fas fa-heart"></span></button>
+      <button className="btn2"><span className="fas fa-hand"></span></button>
+      <h3>Web Design</h3>
+      </div>    
+    <div className="card">
+      <img src="../../fr.jpg" alt="" />
+      <button className="btn1"><span className="fas fa-heart"></span></button>
+      <button className="btn2"><span className="fas fa-hand"></span></button>
+      <h3>Web Design</h3>
+      </div>    
+    <div className="card">
+      <img src="../../fr.jpg" alt="" />
+      <button className="btn1"><span className="fas fa-heart"></span></button>
+      <button className="btn2"><span className="fas fa-hand"></span></button>
+      <h3>Web Design</h3>
+      </div>    
+    <div className="card">
+      <img src="../../fr.jpg" alt="" />
+      <button className="btn1"><span className="fas fa-heart"></span></button>
+      <button className="btn2"><span className="fas fa-hand"></span></button>
+      <h3>Web Design</h3>
+      </div>    
+    <div className="card">
+      <img src="../../fr.jpg" alt="" />
+      <button className="btn1"><span className="fas fa-heart"></span></button>
+      <button className="btn2"><span className="fas fa-hand"></span></button>
+      <h3>Web Design</h3>
+      </div>    
+    <div className="card">
+      <img src="../../fr.jpg" alt="" />
+      <button className="btn1"><span className="fas fa-heart"></span></button>
+      <button className="btn2"><span className="fas fa-hand"></span></button>
+      <h3>Web Design</h3>
+      </div>    
+     </div>
+    </div>
+   );
+}
+ 
 export default Portfolio;
