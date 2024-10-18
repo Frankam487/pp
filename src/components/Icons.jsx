8cb43e6f14a50d294d2565a984a6d254830ec760
Frankam487 <fkamgang06@gmail.com>
2024-10-18 14:23:10 +0100

    responsive

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,10 +1,10 @@
 const Icons = () => {
    return (
       <div className="icons">
-         <a href="http://www.google.com" target="_blank" className="fab fa-facebook"></a>
-         <a href="" className="fab fa-github"></a>
-         <a href="" className="fab fa-linkedin"></a>
-         <a href="" className="fab fa-telegram"></a>
+         <a href="https://www.facebook.com/Frankam.Shopping" target="_blank" className="fab fa-facebook"></a>
+         <a href="https://github.com/Frankam487" target="_blank" className="fab fa-github"></a>
+         <a href="https://www.linkedin.com/in/frank-kamgang/" target="_blank" className="fab fa-linkedin"></a>
+         <a href="https://t.me/Frankam4" target="_blank" className="fab fa-telegram"></a>
       </div>
    );
 }
